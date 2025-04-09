d0a75d9814b76e683503c4cd0d38b678e11f9b37
ZainKhan <73061762+Zain-art@users.noreply.github.com>
2025-04-09 16:23:50 +0500

    update App.js file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. use Docker file and compose up --build command
         </p>
+        <h1>this is update react app at 4:23 pm 9-4-25</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
